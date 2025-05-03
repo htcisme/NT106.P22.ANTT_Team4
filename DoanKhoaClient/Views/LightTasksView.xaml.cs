4a0e60fea8531066019e3b83be40b82cd78fb60a
htcisme <23521673@gm.uit.edu.vn>
2025-05-03 23:20:44 +0700

    Light Dark Helping for DKTXT

---
+++ DoanKhoaClient/Views/LightTasksView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using DoanKhoaClient.Helpers;
+
+namespace DoanKhoaClient.Views
+{
+    public partial class LightTasksView : Window
+    {
+        public LightTasksView()
+        {
+            InitializeComponent();
+            ThemeManager.ApplyTheme(LightTask_Background);
+        }
+
+        private void ThemeToggleButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ThemeManager.ToggleTheme(LightTask_Background);
+        }
+    }
+}
